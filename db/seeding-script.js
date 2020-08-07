0e86c061925e5045cea869e7c466935f6ec41057
Daniel Co <dcohomemail@gmail.com>
2020-08-07 16:55:12 -0700

    create script to seed db with product ids and reviews

---
+++ db/seeding-script.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const fake = require('./fake-data-gen.js')
+
+// db configuration
+mongoose.connect('mongodb://localhost/reviews', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// connect to db
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '))
+db.once('open', () => {
+  console.log('connection successful!');
+})
+
+// set schema
+const reviewSchema = new mongoose.Schema({
+  product_id: String,
+  reviews: [{
+    name: String,
+    rating: Number,
+    date: Date,
+    review: String,
+    purchased_item_name: String,
+    purchased_item_pic: String,
+    attached_pic: String,
+    profile_pic: String
+  }]
+})
+
+// create model
+const Review = mongoose.model('Review', reviewSchema);
+
+// create 100 products and save to db
+const generateProductsToDB = async () => {
+  try {
+    for (let i = 0; i < 100; i++) {
+      // create zero padded product_id
+      const id = i.toString().padStart(5, '0');
+
+      const review = new Review({
+        product_id: id,
+        reviews: fake.createReviewsArray()
+      })
+
+      await review.save()
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    mongoose.disconnect();
+  }
+}
+
+generateProductsToDB()
+
+
+
+
+
+
