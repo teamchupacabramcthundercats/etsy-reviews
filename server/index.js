3d4c9584bd31ad8318ed7f9bb1d9655f91ea5342
Daniel Co <dcohomemail@gmail.com>
2020-08-08 14:47:15 -0700

    set up express server and get route to db

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const db = require('../db/db.js')
+const app = express()
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(morgan('dev'));
+
+app.get('/api/product/:productId', (req, res) => {
+  const { productId } = req.params;
+
+  db.getReviewsById(productId)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
