3cfa62cb7d2a3c0550997d65904deea91d5c1639
Daniel Co <dcohomemail@gmail.com>
2020-08-10 10:56:39 -0700

    set up test for get api route with supertest

--- server/start.js
+++ server/start.js
@@ -1,4 +1,5 @@
-const app = require('./index.js')
+const app = require('./index.js');
+
 const port = 3000;
 
 app.listen(port, () => {
---
+++ tests/server.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../server/index.js');
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
+
+it('endpoint responds with correct object', (done) => (
+  request(app)
+    .get('/api/product/00001')
+    .expect((response) => {
+      response.body.product_id = '00001';
+    })
+    .expect(200, done)
+));
+
+it('endpoint responds with status code 500 with wrong id', async (done) => {
+  const response = await request(app).get('/api/product/12345');
+
+  expect(response.status).toBe(500);
+  done();
+});
