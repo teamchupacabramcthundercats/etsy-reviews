5ab70e41d544047844a75b9eda03c40f69f78e63
Daniel Co <dcohomemail@gmail.com>
2020-08-13 14:37:46 -0700

    implement logic for sorting reviews when selector is used

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,25 +7,42 @@ const App = () => {
   const [review, setReview] = useState([]);
   const [sort, setSort] = useState('recommended');
 
+  const getProductReview = async () => {
+    const path = (window.location.pathname).slice(9);
+
+    const result = await axios(`/api/product/${path}`);
+
+    setReview(result.data.reviews);
+  };
+
+  const sortReviews = (sortMethod) => {
+    let sorted;
+
+    if (sortMethod === 'newest') { // sort by date descending
+      sorted = review.sort((a, b) => (
+        new Date(b.date) - new Date(a.date)
+      ));
+    } else {
+      sorted = review.sort((a, b) => ( // sort by rating descending if sorthMethod is 'recommended'
+        b.rating - a.rating
+      ));
+    }
+
+    setReview(sorted);
+  };
+
   useEffect(() => {
-    const getProductReview = async () => {
-      const path = (window.location.pathname).slice(9);
-
-      const result = await axios(`/api/product/${path}`);
-      setReview(result.data.reviews);
-    };
-
-    getProductReview();
+    if (review.length === 0) {
+      getProductReview();
+    }
   }, []);
-
-  // const sortReviews = () => {
-
-  // };
 
   const handleSortChange = ({ target }) => {
     const { value } = target;
 
     setSort(value);
+
+    sortReviews(value);
   };
 
   return (
